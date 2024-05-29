663f9af2b12fe45334bdcdebfdc2fbca0beb4470
Muhammad Sholehuddin Yusuf <49514986+Yusuf10071998@users.noreply.github.com>
2024-05-29 14:11:02 +0700

    Add files via upload

---
+++ GUI Kriptografi.py
@@ -0,0 +1,194 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+import numpy as np
+from Kriptografi import enkripsi, deskripsi, corelation_value, encryption_quality
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("A Criptography Algorithm Using Quantum Wavelet S-Transform")
+        self.geometry(f"{1100}x{610}")
+
+        # configure grid layout
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((1, 2), weight=1)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=10)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Menu", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Input Data", command=self.sidebar_button_1_event)
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Enkripsi", command=self.sidebar_button_2_event)
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Deskripsi", command=self.sidebar_button_3_event)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, text="Reset", command=self.sidebar_button_4_event)
+        self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_5 = customtkinter.CTkButton(self.sidebar_frame, text="Save", command=self.sidebar_button_event)
+        self.sidebar_button_5.grid(row=5, column=0, padx=20, pady=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+
+        # create mid frame
+        self.mid_frame = customtkinter.CTkFrame(self, width=340, corner_radius=10)
+        self.mid_frame.grid(row=0, column=1, rowspan=13, padx=(10,5), sticky="nsew")
+        self.mid_frame.grid_rowconfigure(0, weight=0)
+        self.mid_frame.grid_columnconfigure(1, weight=1)
+        self.label_plain = customtkinter.CTkLabel(self.mid_frame, text="Plain Text", font=customtkinter.CTkFont(size=13, weight="bold"), justify="left")
+        self.label_plain.grid(row=0, column=1, padx=20, pady=(10, 10))
+        self.textbox_plain = customtkinter.CTkTextbox(self.mid_frame)
+        self.textbox_plain.grid(row=1, column=1, padx=(10, 10), pady=(0, 0), sticky="nsew")
+        self.textbox_plain.insert("0.0", "")
+        self.label_char1 = customtkinter.CTkLabel(self.mid_frame, text="Panjang Karakter", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char1.grid(row=2, column=1, padx=20, pady=(10, 0))
+        self.entry1 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry1.grid(row=3, column=1, padx=10, sticky="nsew")
+        self.label_char2 = customtkinter.CTkLabel(self.mid_frame, text="Kunci Enkripsi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char2.grid(row=4, column=1, padx=20)
+        self.entry2 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry2.grid(row=5, column=1, padx=10, sticky="nsew")
+        self.label_char3 = customtkinter.CTkLabel(self.mid_frame, text="Waktu Enkripsi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char3.grid(row=6, column=1, padx=20)
+        self.entry3 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry3.grid(row=7, column=1, padx=10, sticky="nsew")
+        self.label_char4 = customtkinter.CTkLabel(self.mid_frame, text="Nilai Korelasi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char4.grid(row=8, column=1, padx=20)
+        self.entry4 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry4.grid(row=9, column=1, padx=10, sticky="nsew")
+        self.label_char5 = customtkinter.CTkLabel(self.mid_frame, text="Kualitas Enkripsi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char5.grid(row=10, column=1, padx=20)
+        self.entry5 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry5.grid(row=11, column=1, padx=10, sticky="nsew")
+        self.label_char6 = customtkinter.CTkLabel(self.mid_frame, text="Persentase Kualitas Enkripsi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char6.grid(row=12, column=1, padx=20)
+        self.entry6 = customtkinter.CTkEntry(self.mid_frame)
+        self.entry6.grid(row=13, column=1, padx=10, sticky="nsew")
+
+        # create end frame
+        self.end_frame = customtkinter.CTkFrame(self, width=340, corner_radius=10)
+        self.end_frame.grid(row=0, column=2, rowspan=5, padx=(5,10), sticky="nsew")
+        self.end_frame.grid_rowconfigure(0, weight=0)
+        self.end_frame.grid_columnconfigure(2, weight=1)
+        self.label_chip = customtkinter.CTkLabel(self.end_frame, text="Chiper Text", font=customtkinter.CTkFont(size=13, weight="bold"))
+        self.label_chip.grid(row=0, column=2, padx=20, pady=(10, 10))
+        self.textbox_chip = customtkinter.CTkTextbox(self.end_frame)
+        self.textbox_chip.grid(row=1, column=2, padx=(10, 10), pady=(0, 0), sticky="nsew")
+        self.textbox_chip.insert("0.0", "")
+        self.label_char_1 = customtkinter.CTkLabel(self.end_frame, text="Panjang Karakter", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char_1.grid(row=2, column=2, padx=20, pady=(10, 0))
+        self.entry_1 = customtkinter.CTkEntry(self.end_frame)
+        self.entry_1.grid(row=3, column=2, padx=10, sticky="nsew")
+        self.label_char_2 = customtkinter.CTkLabel(self.end_frame, text="Kunci Deskripsi 1", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char_2.grid(row=4, column=2, padx=20)
+        self.entry_2 = customtkinter.CTkEntry(self.end_frame)
+        self.entry_2.grid(row=5, column=2, padx=10, sticky="nsew")
+        self.label_char_3 = customtkinter.CTkLabel(self.end_frame, text="Kunci Deskripsi 2", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char_3.grid(row=6, column=2, padx=20)
+        self.entry_3 = customtkinter.CTkEntry(self.end_frame)
+        self.entry_3.grid(row=7, column=2, padx=10, sticky="nsew")
+        self.label_char_4 = customtkinter.CTkLabel(self.end_frame, text="Waktu Deskripsi", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char_4.grid(row=8, column=2, padx=20)
+        self.entry_4 = customtkinter.CTkEntry(self.end_frame)
+        self.entry_4.grid(row=9, column=2, padx=10, sticky="nsew")
+        self.label_char_5 = customtkinter.CTkLabel(self.end_frame, text="Similarity", font=customtkinter.CTkFont(size=11, weight="bold"))
+        self.label_char_5.grid(row=10, column=2, padx=20)
+        self.entry_5 = customtkinter.CTkEntry(self.end_frame)
+        self.entry_5.grid(row=11, column=2, padx=10, sticky="nsew")
+
+         # create end frame
+        self.ended_frame = customtkinter.CTkFrame(self, width=340, corner_radius=10)
+        self.ended_frame.grid(row=0, column=3, rowspan=5, padx=(5,10), sticky="nsew")
+        self.ended_frame.grid_rowconfigure(0, weight=0)
+        self.ended_frame.grid_columnconfigure(2, weight=1)
+        self.label_invchip = customtkinter.CTkLabel(self.ended_frame, text="Plain Text", font=customtkinter.CTkFont(size=13, weight="bold"))
+        self.label_invchip.grid(row=0, column=3, padx=20, pady=(10, 10))
+        self.textbox_invchip = customtkinter.CTkTextbox(self.ended_frame)
+        self.textbox_invchip.grid(row=1, column=3, padx=(10, 10), pady=(0, 0), sticky="nsew")
+        self.textbox_invchip.insert("0.0", "")
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+    def sidebar_button_1_event(self):
+        file_path = tkinter.filedialog.askopenfilename()
+        if file_path:
+            with open(file_path, 'r') as file:
+                data = file.read()
+                self.textbox_plain.delete("1.0", "end")
+                self.textbox_plain.insert("1.0", data)
+                self.entry1.delete(0, "end")
+                self.entry1.insert(0, str(len(data)))
+
+    def sidebar_button_4_event(self):
+        self.textbox_plain.delete("1.0", "end")
+        self.textbox_chip.delete("1.0", "end")
+        self.entry1.delete(0, "end")
+        self.entry2.delete(0, "end")
+        self.entry3.delete(0, "end")
+        self.entry4.delete(0, "end")
+        self.entry5.delete(0, "end")
+        self.entry6.delete(0, "end")
+        self.entry_1.delete(0, "end")
+        self.entry_2.delete(0, "end")
+        self.entry_3.delete(0, "end")
+        self.entry_4.delete(0, "end")
+        self.entry_5.delete(0, "end")
+
+    def sidebar_button_2_event(self):
+        bit_precision = 4  # 6 bit precision needs 64G
+        bit_precision += 2
+        Plain_text = self.textbox_plain.get("1.0", "end-1c")
+        key_input = self.entry2.get()
+        Key = [int(k) for k in key_input.split(',')]
+        chiper_text, key2, te = enkripsi(Plain_text, Key, bit_precision)
+        self.textbox_chip.delete("1.0", "end")
+        self.textbox_chip.insert("1.0", chiper_text)
+        self.entry3.delete(0, "end")
+        self.entry3.insert(0, te)
+        self.entry_1.delete(0, "end")
+        self.entry_1.insert(0, str(len(chiper_text)))
+        self.entry_2.delete(0, "end")
+        self.entry_2.insert(0, ', '.join(map(str, Key)))
+        self.entry_3.delete(0, "end")
+        self.entry_3.insert(0, ', '.join(map(str, key2)))
+        nilai_korelasi = corelation_value(Plain_text, chiper_text)
+        self.entry4.delete(0, "end")
+        self.entry4.insert(0, nilai_korelasi)
+        max_quality_enk, persent_quality_enk = encryption_quality(Plain_text, chiper_text)
+        self.entry5.delete(0, "end")
+        self.entry5.insert(0, max_quality_enk)
+        self.entry6.delete(0, "end")
+        self.entry6.insert(0, persent_quality_enk)
+
+    def sidebar_button_3_event(self):
+        bit_precision = 4
+        bit_precision += 2
+        ChiperText = self.textbox_chip.get("1.0", "end-1c")
+        key_input = self.entry_2.get()
+        Key = [int(k) for k in key_input.split(',')]
+        key2_input = self.entry_3.get()
+        Key2 = [int(k) for k in key2_input.split(',')]
+        SignalOri = np.array([ord(char) for char in self.textbox_plain.get("1.0", "end-1c")]) 
+        invChipperText, td = deskripsi(ChiperText, Key2, Key, bit_precision, SignalOri)
+        self.textbox_invchip.insert("1.0", invChipperText)
+        self.entry_4.insert(0, td)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ Kriptografi.py
@@ -0,0 +1,404 @@
+from qiskit import*
+from qiskit.visualization import plot_histogram
+from qiskit.circuit.library import SXdgGate
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import qiskit_aer
+import time
+
+
+csxdg_gate = SXdgGate().control() #To generate the V and V*- gates
+getbinary = lambda x, n: format(x, 'b').zfill(n) #to get the binary representation with n-bit precision
+
+def AddOperator(t,qc,add_v,s):
+    for n in range(t):
+        if n<t-1:
+            qc.append(csxdg_gate, [s[n+t],   add_v[n+1]])
+            qc.append(csxdg_gate, [s[n],     add_v[n+1]])
+            qc.append(csxdg_gate, [add_v[n], add_v[n+1]])
+
+            qc.cx(s[n],   s[n+t])
+            qc.cx(s[n+t], add_v[n])
+            qc.cx(s[n],   s[n+t])
+
+            qc.csx(add_v[n], add_v[n+1])
+            qc.barrier()
+        else: #The carry bit is discarded
+            qc.cx(s[n],   s[n+t])
+            qc.cx(s[n+t], add_v[n])
+            qc.cx(s[n],   s[n+t])
+            qc.barrier()
+
+    return qc
+
+def SubOperator(t,qc,sub_v,s):
+
+    qc.append(csxdg_gate, [s[t], sub_v[0]])
+    qc.cx(s[0] , s[t]) #Here we discard the 0 position of the add_v variable (it is equal to subtract 1)
+    qc.csx(s[0], sub_v[0])
+    qc.csx(s[t], sub_v[0])
+
+    qc.barrier()
+
+    for n in range(1,t,1):
+        if n<t-1:
+            qc.append(csxdg_gate, [s[n+t], sub_v[n]])
+            qc.cx(s[n],  s[n+t])
+            qc.csx(s[n], sub_v[n])
+            qc.cx(sub_v[n-1],  s[n+t])
+
+            qc.csx(sub_v[n-1], sub_v[n])
+            qc.csx(s[n+t],   sub_v[n])
+
+            qc.barrier()
+        else:
+            qc.cx(s[n],  s[n+t])
+            qc.cx(sub_v[n-1],  s[n+t])
+            qc.barrier()
+
+    return qc
+
+#Two complement function - For integer numbers
+def TwoComplementI(number, bit_precision):
+    t = getbinary(int(number), bit_precision+1)
+    temp = int(t, 2)
+    inverse_s = temp ^ (2 ** (len(t) + 1) - 1)
+    rslt = bin(inverse_s)[3 : ]
+
+    b = '01'
+    sum = bin(int(rslt, 2) + int(b, 2))
+    sum = int(sum,2)
+
+    return (sum)
+
+#Two complement function - For binary numbers
+def TwocomplementB(number):
+
+    temp = int(number, 2)
+    inverse_s = temp ^ (2 ** (len(number) + 1) - 1)
+    rslt = bin(inverse_s)[3 : ] 
+
+    b = '01'
+    sum = bin(int(rslt, 2) + int(b, 2))
+    
+
+    return (sum[2:]) 
+
+def QCircuit(bit_precision):
+    #-------Register initialization-------
+    s = QuantumRegister(2*bit_precision,'s')
+            
+    #Addition variable
+    add_v = QuantumRegister(bit_precision + 1,'a') #The +1 is for the rounding operation
+    #Classical register 
+    cr = ClassicalRegister(bit_precision*2,'c')
+    #Subtraction variable
+    sub_v = QuantumRegister(bit_precision-1,'d')
+    
+    #Circuit definition 
+    qc = QuantumCircuit(s,add_v, sub_v, cr)    
+    
+    return (qc, add_v, sub_v, s)
+
+#Simulation process
+def FSimulation(circuit):
+
+    simulator = qiskit_aer.Aer.get_backend('qasm_simulator')
+    job = transpile(circuit, backend=simulator)
+    result = simulator.run(job).result()
+    counts = result.get_counts() #Simulation results
+    
+    #display(result.get_counts(circuit))
+    return(counts)
+
+def initialComplement(a,b, bit_precision):
+
+    p = ''
+
+    if a<0:
+        #Signal[2*i] = TwoComplementI(Signal[2*i]*-1,bit_precision-1)
+        p = getbinary(int(TwoComplementI(a*-1,bit_precision-1)), bit_precision)[::-1]
+    else:
+        p =  getbinary(int(a), bit_precision)[::-1]        
+
+    if b<0:
+        #Signal[2*i+1] = TwoComplementI(Signal[2*i+2]*-1,bit_precision-1)
+        p = p + getbinary(int(TwoComplementI(b*-1,bit_precision-1)), bit_precision)[::-1]
+    else:
+        p = p + getbinary(int(b), bit_precision)[::-1]
+    
+    return p
+
+def QISTransform(Signal, DLevel, bit_precision):
+
+    #Initial Zero padding (it is because we need a 2^n signal elements)
+    while (Signal.size)%2!=0 or (Signal.size)<2:
+        Signal = np.append(Signal,0)
+
+    # Exception
+    max_depth = len(Signal)//2
+    if DLevel> max_depth:
+      raise Exception("Sorry, depth nya kakehan")
+
+    #Decomposition level
+    list_AD = []
+
+    for level in range(0, DLevel):
+        if level>0:
+            Signal = np.array(ai)
+            if Signal.size%2==1:
+                Signal = np.append(Signal,0)
+        di  = [] #to store the D(i) coefficient
+        ai  = [] #to store the A(i) coefficient
+        for i in range(0,int(Signal.size/2),1):
+
+            #Take the next three elements of the signal
+            signal = [Signal[2*i], Signal[2*i+1]]
+
+            #Two's complement to the initial signal elements (return a string with the binary representation)
+            p = initialComplement(Signal[2*i], Signal[2*i+1], bit_precision)
+
+            #Circuit definition
+            (circuit,add_v,sub_v,s) = QCircuit(bit_precision)
+
+            #-------Signal elements storing-------
+            #Apply the X gate when the bit is 1, and I gate when is 0
+            if [j for j in range(len(p)) if p[j]=='1'] !=[]:
+                circuit.x( [j for j in range(len(p)) if p[j]=='1'])
+            circuit.barrier()
+
+            #-------Operators-------
+            #Addition operation -- Between the S(2i) and S(2i+1) signal elements
+            circuit = AddOperator(bit_precision, circuit, add_v, s)
+
+            #For the rounding - Copy the MSB into the next qubit
+            circuit.cx(add_v[-2],add_v[-1])
+            circuit.barrier()
+
+            #Subtraction operation -- Between the S(2i+1) and the S(2i) element
+            circuit = SubOperator(bit_precision, circuit, sub_v, s)
+
+            #-------Measurement-------
+            r_1 = [add_v[o] for o in range(1,bit_precision+1,1)]
+            r_2 = [s[o] for o in range(bit_precision, 2*bit_precision,1)]
+            r = np.append(r_1,r_2)
+
+            m = range(bit_precision*2) #Number of classical bits
+
+            circuit.measure(r, m)
+
+            #-------Simulation-------
+            d = str(FSimulation(circuit))
+            #print(d)
+
+            #Extract the binary representation
+            a_1 = d[2+bit_precision:2*bit_precision+2]
+            #print(a_1)
+            d_1 = d[2:2+bit_precision]
+
+            #Two's complement of the number
+            if d_1[0]=='1':
+                d_1 = int(TwocomplementB(d_1),2)*-1
+            else:
+                d_1 = int(d_1, 2)
+
+            if a_1[0]=='1':
+                a_1 = int(TwocomplementB(a_1),2)*-1
+            else:
+                a_1 = int(a_1, 2)
+
+            #Store the coefficient D(i) in the variable
+            ai.append(a_1)
+            di.append(d_1)
+
+        list_AD.append(di)
+
+        if level==(DLevel-1):
+            list_AD.append(ai)
+    list_AD = list_AD[::-1]
+    # display(circuit.draw("mpl"))
+
+    return list_AD
+
+def InvQISTransform(input, bit_precision):
+    DLevel = len(input) - 1
+    Ai = np.array(input[0])
+    Di = np.array(input[1])
+    for level in range(DLevel):
+        ai  = [] #to store the A(i) coefficient
+        for i in range(Ai.size):
+            #Take the next three elements of the signal
+            signal = [Di[i], 2*Ai[i]]
+            if Di[i]%2==1:
+                signal = [Di[i], 2*Ai[i]+1]
+
+            #Two's complement to the initial signal elements (return a string with the binary representation)
+            p = initialComplement(signal[0], signal[1], bit_precision)
+
+            #Circuit definition
+            (circuit,add_v,sub_v,s) = QCircuit(bit_precision)
+
+            #-------Signal elements storing-------
+            #Apply the X gate when the bit is 1, and I gate when is 0
+            if [j for j in range(len(p)) if p[j]=='1'] !=[]:
+                circuit.x( [j for j in range(len(p)) if p[j]=='1'])
+            circuit.barrier()
+
+            #-------Operators-------
+            #Addition operation -- Between the S(2i) and S(2i+1) signal elements
+            circuit = AddOperator(bit_precision, circuit, add_v, s)
+
+            #For the rounding - Copy the MSB into the next qubit
+            circuit.cx(add_v[-2],add_v[-1])
+            circuit.barrier()
+
+            #Subtraction operation -- Between the S(2i+1) and the S(2i) element
+            circuit = SubOperator(bit_precision, circuit, sub_v, s)
+
+            #-------Measurement-------
+            r_1 = [add_v[o] for o in range(1,bit_precision+1,1)]
+            r_2 = [s[o] for o in range(bit_precision, 2*bit_precision,1)]
+            r = np.append(r_1,r_2)
+
+            m = range(bit_precision*2) #Number of classical bits
+
+            circuit.measure(r, m)
+
+            #-------Simulation-------
+            d = str(FSimulation(circuit))
+
+            #Extract the binary representation
+            a_1 = d[2+bit_precision:2*bit_precision+2]
+
+            d_1 = d[2:2+bit_precision]
+
+            #Two's complement of the number
+            if d_1[0]=='1':
+                d_1 = int(TwocomplementB(d_1),2)*-1
+            else:
+                d_1 = int(d_1, 2)
+
+            if a_1[0]=='1':
+                a_1 = int(TwocomplementB(a_1),2)*-1
+            else:
+                a_1 = int(a_1, 2)
+
+            ai.append(d_1//2)
+            ai.append(a_1)
+
+        Ai = np.array(ai)
+        if Ai[-1]==0:
+            Ai = Ai[:-1]
+        if level<DLevel-1:
+            Di = input[level+2]
+    return Ai
+
+def QISTransformLevel(Signal, Key, bit_precision):
+  for i in range(len(Key)):
+      list_result = QISTransform(Signal, Key[i], bit_precision)
+      Signal = np.concatenate(list_result).ravel()
+  return list_result
+
+def InvQISTransformLevel(inv_result, Key, bit_precision):
+  Key = Key[::-1]
+  for i in range(len(Key)):
+      list_result = []
+      for j in range(Key[i]):
+          list_result.append(inv_result[-(len(inv_result)//2):])
+          inv_result = inv_result[:-(len(inv_result)//2)]
+      list_result.append(inv_result)
+      list_result = list_result[::-1]
+      inv_result = InvQISTransform(list_result, bit_precision)
+  return inv_result
+
+def enkripsi(PlainText, Key, bit_precision):    
+    start_time = time.time()
+    SignalOri = np.array([ord(char) for char in PlainText])
+    Signal = SignalOri % 8
+    list_result = QISTransformLevel(Signal, Key, bit_precision)
+    arr_result = np.concatenate(list_result).ravel()
+    Key2 = np.where(arr_result < 0, 1, 0)
+    X = abs(arr_result) + 32
+    ChiperText = ''.join(chr(x) for x in X)
+    te = round(time.time() - start_time, 2)
+    
+    return ChiperText, Key2, te
+
+def deskripsi(ChiperText, Key2, Key, bit_precision, SignalOri):
+    start_time = time.time()
+    signalChip = np.array([ord(char) for char in ChiperText]) - 32
+    signalChip = np.where(Key2 == 1, -signalChip, signalChip)
+    invChipper = InvQISTransformLevel(signalChip, Key, bit_precision) + (SignalOri // 8) * 8
+    invChipperText = ''.join([chr(x) for x in invChipper])
+    td = round(time.time() - start_time, 2)
+    
+    return invChipperText, td
+
+# PlainText = input("Input Plaintext: ")
+# SignalOri = np.array([ord(char) for char in PlainText])
+# key_input = input("Input Kunci Enkripsi (pisahkan dengan koma): ")
+# Key = [int(k) for k in key_input.split(',')]
+
+# # Bit precision dan depth
+# bit_precision = 4  # 6 bit precision needs 64G
+# bit_precision += 2  # Add two more bit to operate with negative numbers
+
+# # Enkripsi
+# ChiperText, Key2, te = enkripsi(SignalOri, Key, bit_precision)
+# print("PlainText : ", PlainText)
+# print("ChipperText : ", ChiperText)
+# print("Waktu Enkripsi : ", te)
+
+# # Deskripsi
+# invChipperText, td = deskripsi(ChiperText, Key2, Key, bit_precision, SignalOri)
+# print("InvChipperText : ", invChipperText)
+# print("Waktu Deskripsi : ", td)
+
+def corelation_value(PlainText, ChiperText):
+    plain_num = np.array([ord(char) for char in PlainText])
+    chip_num = np.array([ord(char) for char in ChiperText])
+    n = plain_num.size
+    m = chip_num.size
+    A = 0
+    for i in range(min(n, m)):
+        A += plain_num[i] * chip_num[i]
+    A = n * A
+    PlainText = 0
+    ChiperText = 0
+    for i in range(min(n, m)):
+        PlainText += plain_num[i]
+        ChiperText += chip_num[i]
+    B = PlainText * ChiperText
+    plainS = 0
+    chipS = 0
+    for i in range(min(n, m)):
+        plainS += plain_num[i] ** 2
+        chipS += chip_num[i] ** 2
+    nilai_korelasi = (A - B) / (np.sqrt((n * plainS - PlainText ** 2) * (n * chipS - ChiperText ** 2)))
+    nilai_korelasi = round(abs(nilai_korelasi),5)
+    return nilai_korelasi
+
+def count_characters(char, text):
+    return text.count(char)
+
+def encryption_quality(PlainText, ChiperText):
+    temp = 0
+    n = 256 - 32
+    
+    for i in range(32, 256):
+        temp += abs(count_characters(chr(i), PlainText) - count_characters(chr(i), ChiperText))
+    
+    result = temp / n
+    n1 = len(PlainText)
+    n2 = len(ChiperText)
+    
+    if 2 * n1 < n2:
+        max_quality_enk = round(2 * n2 / n,5)
+    else:
+        max_quality_enk = round(2 * n2 / n,5)
+    
+    persent_quality_enk = round((result / max_quality_enk) * 100,3)
+    
+    return max_quality_enk, persent_quality_enk
